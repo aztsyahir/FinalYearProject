e93761aed3b837a292534964efa96cb3c53e1596
aztsyahir <2021603394@student.uitm.edu.my>
2024-02-15 20:58:14 +0800

    latest updated

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -20,7 +20,7 @@ public class GettingStartedApplication {
     public GettingStartedApplication(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
+//h
     @GetMapping("/")
     public String index() {
         return "Signin";
